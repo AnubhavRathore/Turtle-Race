3d04761b2a10b085850959943ad54821e1242439
AnubhavRathore <86161584+AnubhavRathore@users.noreply.github.com>
2021-10-02 16:45:01 +0530

    Update player.py

--- player.py
+++ player.py
@@ -17,18 +17,12 @@ class Player(Turtle):
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
-        """below is my own method to move turtle up"""
-        # new_y = self.ycor() + MOVE_DISTANCE
-        # self.goto(self.xcor(), new_y)
 
     def is_at_finish_line(self):
         if self.ycor() > FINISH_LINE_Y:
             return True
         else:
             return False
-    '''My own method to reach starting position'''
+    '''Method to reach starting position'''
     def restart(self):
         self.goto(STARTING_POSITION)
-
-
-
